6513d23602eff135083a54752689c731fd78399d
Pedro Henrique <138126897+Mechph@users.noreply.github.com>
2024-08-28 15:20:50 -0300

    Add files via upload

---
+++ front-end/CadastroUsuario.js
@@ -0,0 +1,11 @@
+function CadastrarUsuario() {
+    return(
+        <div>
+            <h3>Não possui uma conta ?</h3>
+            <button type="submit">Fazer Cadastro</button>
+            
+        </div>
+    )
+}
+
+export default CadastrarUsuario
