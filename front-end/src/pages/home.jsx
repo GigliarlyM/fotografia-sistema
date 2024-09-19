918ebd01d35c3fe632e116094048ca8173b232c9
Ademário de Sousa Neto <ademarionetoooo@gmail.com>
2024-09-19 17:50:05 -0300

    ADICIONADO-FOOTER-AJUSTE-NA-HOME

--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -3,23 +3,26 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Photo } from "./get-photo";
 
+
 export default function Home() {
     const navigate = useNavigate()
     
     return (
         <>
-            <div>
+            <div className="button-container">
                 <button onClick={() => navigate("login/photographer")}>Entrar como fotografo</button>
                 <button onClick={() => navigate("login/client")}>Entrar como Cliente</button>
                 <button onClick={() => navigate("signup")}>Cadastrar</button>
             </div>
-            <div>
+            <div className="text-container">
                 <h1>Bem-vindo ao FotoHub !</h1>
                 <p>Estamos felizes em tê-lo aqui, onde talento e criatividade se encontram!<br/> Quer você seja um fotógrafo em busca de novos projetos ou um cliente à procura do profissional perfeito<br/> para capturar seus momentos especiais, você está no lugar certo.<br/> Explore, descubra e conecte-se com a arte da fotografia.</p>
             </div>
             <div className="photos">
                 <RenderPhotos />
             </div>
+            <footer className="footer"> &copy; 2024 FotoHub - Todos os direitos reservados</footer>
+
         </>
     )
 }
