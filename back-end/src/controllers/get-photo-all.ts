e0eb03de6400f8b8d3a7fd135df638f9d0215773
GIGLIARLY MARCELINO GONZAGA <2313081138@CESED.LOCAL>
2024-09-26 10:05:19 -0300

    melhoras back

--- back-end/src/controllers/get-photo-all.ts
+++ back-end/src/controllers/get-photo-all.ts
@@ -6,12 +6,8 @@ import { getPhotoAllModel } from "../model/service";
 export default async function getPhotoAll(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().get('/photo', {
     }, async () => {
-        try {
-            const photos = await getPhotoAllModel()
-            
-            return { photos }
-        } catch (err) {
-            return { message: err}
-        }
+        const photos = await getPhotoAllModel()
+
+        return { photos }
     })
 }
--- back-end/src/model/service-shcema.ts
+++ back-end/src/model/service-shcema.ts
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import { string } from "zod";
 
 const service = new mongoose.Schema({
     id: {
